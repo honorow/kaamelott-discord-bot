7a55285ba037534cf7f5a8d88a6af3b9ef6cc085
honorow <63323985+honorow@users.noreply.github.com>
2020-04-07 18:43:42 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.Njk3MjMxOTE3MzIxNDIwOTAx.Xo0SHA.VSDNsdBpoNcaL8R4vKDBskjJiM0);//BOT_TOKEN is the Client Secret
